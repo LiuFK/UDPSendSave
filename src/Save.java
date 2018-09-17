ebecc081bb488a1cc8fb94613ab7970466074d06
LiuFK <992036303@qq.com>
2018-09-17 19:32:36 +0800

    保存数据

--- src/Save.java
+++ src/Save.java
@@ -7,23 +7,19 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class Save {
-
 	public static void main(String[] args) throws IOException {
-		//定义一个接收端，并且指定了接收的端口号
-		DatagramSocket socket=new DatagramSocket(8080);
-		
+		DatagramSocket socket=new DatagramSocket(8083);
 		while(true) {
 			byte[] buf=new byte[1024];
 			//解析数据包
 			DatagramPacket packet=new DatagramPacket(buf, buf.length);
 			socket.receive(packet);
 			String ip=packet.getAddress().getHostAddress();
+			//String ip="10.138.109.193";
 			buf=packet.getData();
 			String data=new String(buf,0, packet.getLength());
 			System.out.println("收到"+ip+"发送来的消息"+data);
 		}
-		
 		//System.out.println(InetAddress.getLocalHost().toString());
 	}
-
 }
